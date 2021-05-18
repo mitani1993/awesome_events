21c43ae088d5b8d1b8a460bfdaaf65e7862e9204
mitani1993 <fytim55@gmail.com>
2021-05-18 21:11:54 +0900

    更新、削除機能

--- app/models/event.rb
+++ app/models/event.rb
@@ -10,6 +10,11 @@ class Event < ApplicationRecord
   end
 
   validate :start_at_should_be_before_end_at
+
+  def created_by?(user)
+    return false unless user
+    owner_id == user.id
+  end
   
   private
 
