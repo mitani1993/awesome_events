b463f531c8cf38cd714e297a9080684052afdfa7
mitani1993 <fytim55@gmail.com>
2021-05-19 21:37:55 +0900

    退会機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "auth/:provider/callback", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+  resource :retirements, only: %i[new create]
   resources :events do
     resources :tickets
   end
